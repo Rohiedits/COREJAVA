470f277e8793a8ccdf659d1e9c850e90c22ccc89
Rohiedits <142705213+Rohiedits@users.noreply.github.com>
2024-09-20 23:01:35 +0530

    first commit

---
+++ sample.java
@@ -0,0 +1,60 @@
+class rohi {
+    public static void main(String a[]) {
+        // byte by = 129; // error because byte starts from -128 to 127 like (-2^7
+        // to2^7-1 )
+        // char name = 'm';// char difine only single character
+        // int p = 1;
+        // // int q = 5;
+        // float f = 9.72;
+        // String name = "rohi";//entire name can define in strings
+        // boolean have = true;
+        // while (p <= 10) {
+        // // int result = p * q;
+        // // System.out.println(p + "." + "Rohith ");
+        // // System.out.println(5 + "X " + p + "=" + result);
+        // p -= -1;
+        // name++;
+        // int num = 10_00_000;//litters
+        // byte b = 127;
+        // int num1 = 12;
+        // // b = num1; // type converstion in WRONG beacuse b is byte can't stoned in
+        // num1
+        // num1 = b;// type converstion in CORRECT manner
+
+        // System.out.println(num1);
+
+        // TYPECASTING ---------------------------------
+        // byte c = 10;
+        // byte b = 50;
+        // int result = c * b;
+        // System.out.println(result);
+
+        // assign-operator
+        // int num1 = 5;
+        // int num2 = 6;
+        // num1 -= -5;
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        // int num = 7;
+        // num++;// 8 post-increment
+        // ++num;// 8 pre-increment
+        // System.out.println(num);
+
+        // boolean op
+        // int x = 10;
+        // int y = 20;
+        // boolean result = x > y;
+        // System.out.println(result);
+
+        // logical operators
+        // int e = 10;
+        // int b = 20;
+        // int c = 50;
+        // int d = 100;
+        // boolean result = e < b || b < c;//OR operator // e < b && b < c AND operator
+        // // e < b ! b < c NOT operator
+        // System.out.println(result);
+
+    }
+}
